28926da9ab721a97b132998670fbe5d08d136755
hank <stonehank310@gmail.com>
2018-10-14 20:31:15 +0800

    优化antd-icon按需加载

--- config-overrides.js
+++ config-overrides.js
@@ -1,16 +1,20 @@
 const { injectBabelPlugin } = require('react-app-rewired');
-const rewireWebpackBundleAnalyzer = require('react-app-rewire-webpack-bundle-analyzer')
+// const rewireWebpackBundleAnalyzer = require('react-app-rewire-webpack-bundle-analyzer')
+const path=require("path")
 
 module.exports = function override(config, env) {
+  // antd Icon的临时解决方案
+  // https://github.com/ant-design/ant-design/issues/12011#issuecomment-420038579
+  config.resolve.alias['@ant-design/icons/lib/dist$']=path.resolve(__dirname, './src/icons.js')
   config = injectBabelPlugin(
     ['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }],
     config,
   );
-  if (env === 'production') {
-    config = rewireWebpackBundleAnalyzer(config, env, {
-      analyzerMode: 'static',
-      reportFilename: 'report.html'
-    })
-  }
+  // if (env === 'production') {
+  //   config = rewireWebpackBundleAnalyzer(config, env, {
+  //     analyzerMode: 'static',
+  //     reportFilename: 'report.html'
+  //   })
+  // }
   return config;
 };
---
+++ src/icons.js
@@ -0,0 +1,119 @@
+// export what you need
+export {
+  default as TableOutline
+} from '@ant-design/icons/lib/outline/TableOutline';
+
+export {
+  default as ProfileOutline
+} from '@ant-design/icons/lib/outline/ProfileOutline';
+
+export {
+  default as UserOutline
+} from '@ant-design/icons/lib/outline/UserOutline';
+
+export {
+  default as HomeOutline
+} from '@ant-design/icons/lib/outline/HomeOutline';
+
+export {
+  default as DatabaseOutline
+} from '@ant-design/icons/lib/outline/DatabaseOutline';
+
+export {
+  default as ClusterOutline
+} from '@ant-design/icons/lib/outline/ClusterOutline';
+
+
+
+// export what antd other components need
+export {
+  default as CloseOutline
+} from '@ant-design/icons/lib/outline/CloseOutline';
+export {
+  default as CheckOutline
+} from '@ant-design/icons/lib/outline/CheckOutline';
+export {
+  default as LoadingOutline
+} from '@ant-design/icons/lib/outline/LoadingOutline';
+export {
+  default as CheckCircleOutline
+} from '@ant-design/icons/lib/outline/CheckCircleOutline';
+export {
+  default as InfoCircleOutline
+} from '@ant-design/icons/lib/outline/InfoCircleOutline';
+export {
+  default as CloseCircleOutline
+} from '@ant-design/icons/lib/outline/CloseCircleOutline';
+export {
+  default as ExclamationCircleOutline
+} from '@ant-design/icons/lib/outline/ExclamationCircleOutline';
+export {
+  default as CheckCircleFill
+} from '@ant-design/icons/lib/fill/CheckCircleFill';
+export {
+  default as InfoCircleFill
+} from '@ant-design/icons/lib/fill/InfoCircleFill';
+export {
+  default as CloseCircleFill
+} from '@ant-design/icons/lib/fill/CloseCircleFill';
+export {
+  default as ExclamationCircleFill
+} from '@ant-design/icons/lib/fill/ExclamationCircleFill';
+export { default as UpOutline } from '@ant-design/icons/lib/outline/UpOutline';
+export {
+  default as DownOutline
+} from '@ant-design/icons/lib/outline/DownOutline';
+export {
+  default as LeftOutline
+} from '@ant-design/icons/lib/outline/LeftOutline';
+export {
+  default as RightOutline
+} from '@ant-design/icons/lib/outline/RightOutline';
+export {
+  default as RedoOutline
+} from '@ant-design/icons/lib/outline/RedoOutline';
+export {
+  default as CalendarOutline
+} from '@ant-design/icons/lib/outline/CalendarOutline';
+export {
+  default as SearchOutline
+} from '@ant-design/icons/lib/outline/SearchOutline';
+export {
+  default as BarsOutline
+} from '@ant-design/icons/lib/outline/BarsOutline';
+export {
+  default as StarFill
+} from '@ant-design/icons/lib/fill/StarFill';
+export {
+  default as FilterOutline
+} from '@ant-design/icons/lib/outline/FilterOutline';
+export {
+  default as CaretUpOutline
+} from '@ant-design/icons/lib/outline/CaretUpOutline';
+export {
+  default as CaretDownOutline
+} from '@ant-design/icons/lib/outline/CaretDownOutline';
+export {
+  default as PlusOutline
+} from '@ant-design/icons/lib/outline/PlusOutline';
+export {
+  default as FileOutline
+} from '@ant-design/icons/lib/outline/FileOutline';
+export {
+  default as FolderOpenOutline
+} from '@ant-design/icons/lib/outline/FolderOpenOutline';
+export {
+  default as FolderOutline
+} from '@ant-design/icons/lib/outline/FolderOutline';
+export {
+  default as PaperClipOutline
+} from '@ant-design/icons/lib/outline/PaperClipOutline';
+export {
+  default as PictureOutline
+} from '@ant-design/icons/lib/outline/PictureOutline';
+export {
+  default as EyeOutline
+} from '@ant-design/icons/lib/outline/EyeOutline';
+export {
+  default as DeleteOutline
+} from '@ant-design/icons/lib/outline/DeleteOutline';
