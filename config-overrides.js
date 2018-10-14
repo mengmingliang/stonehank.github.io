ecbca21166341282f61ebac1e14c00c16db63eb3
hank <stonehank310@gmail.com>
2018-10-14 14:56:27 +0800

    add bundleAnalyzer

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,16 @@
 const { injectBabelPlugin } = require('react-app-rewired');
+const rewireWebpackBundleAnalyzer = require('react-app-rewire-webpack-bundle-analyzer')
 
 module.exports = function override(config, env) {
   config = injectBabelPlugin(
     ['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }],
     config,
   );
+  if (env === 'production') {
+    config = rewireWebpackBundleAnalyzer(config, env, {
+      analyzerMode: 'static',
+      reportFilename: 'report.html'
+    })
+  }
   return config;
 };
